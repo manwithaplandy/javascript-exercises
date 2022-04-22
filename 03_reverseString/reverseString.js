5cb7555b0f81c2762e3b1c688d29c80db39127c9
Andrew <abysmalduck@gmail.com>
2022-04-22 16:33:36 -0700

    Challenge completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let strLength = inputString.length;
+    let i = strLength - 1;
+    let newStringArray = [];
+    while (i >= 0) {
+        newStringArray.push(inputString.charAt(i));
+        i--;
+    }
+    return newStringArray.join('');
 };
 
 // Do not edit below this line
