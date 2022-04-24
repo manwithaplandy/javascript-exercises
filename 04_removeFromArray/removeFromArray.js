d81deaa0f41930759d100027839eab59928dc849
Andrew <abysmalduck@gmail.com>
2022-04-24 10:55:49 -0700

    First attempt using foreach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+/*
+Take in array and ...args as inputs
+Turn args into array
+For each arg, check if it's in input array
+    If in input array, remove number from input array
+    else, continue to next arg
+Return final array
+*/
+
+let newArray = inputArray;
+let myArgs = args; // Turn args into array
+
+args.forEach(checkAndReplace(value));
+
+function checkAndReplace(num) {
+    if (newArray.includes(num)) {
+        let index = newArray.findIndex(num);
+        newArray.splice(index, 1);
+    }
+    return newArray;
+}
+
+return newArray;
 
 };
 
